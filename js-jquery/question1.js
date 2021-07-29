11c9059d6e972bb1034edefacef020919c0b83e5
bhaveshlokhande-000 <72037462+bhaveshlokhande-000@users.noreply.github.com>
2021-07-29 13:30:19 +0530

    format string

--- js-jquery/question1.js
+++ js-jquery/question1.js
@@ -1,6 +1,29 @@
-let paragraph = `Thank yous wevk k. Dreamland guest relations.f f with Ronaldo. May I have your name, please? Yes, sure. My name is robin soleimani. What is yourphone number? It is 9876535362 as mentioned in the book Diary. Thank you so much for providing the info!`;
+let paragraph = `Thank you so much for contacting us. Dreamland guest relations. You're speaking with Ronaldo. May I have your name, please? Yes, sure. My name is robin soleimani. What is your
+phone number? It is 9876535362 as mentioned in the book Diary. Thank you so much for providing the info!`
 let patt = /\d{10}/gm;
-let formatSentenses = paragraph.replace(patt, "XXXXXXXXXX").split(".");
-let indexCounter = 1;
-formatSentenses.forEach((sentense, index) => sentense.split(" ").length > 3 && console.log(`${indexCounter++}. ${sentense}.`));
-
+let words = paragraph.split(" ");
+let formatSentenses = [];
+let counter = 1;
+let temp_str = "";
+for (let index = 0; index < words.length; index++) {
+    let word = words[index];
+    if (patt.test(word)) {
+        temp_str += "xxxxxx ";
+        continue;
+    }
+    else {
+        temp_str = temp_str + word + " ";
+    }
+    let lastCharacter = word[word.length - 1];
+    if (lastCharacter == "?" || lastCharacter == "." || lastCharacter == "!") {
+        // console.log(temp_str,counter)
+        if (counter > 3) {
+            formatSentenses.push(temp_str.substr(0, temp_str.length - 1));
+        }
+        counter = 0;
+        temp_str = "";
+    }
+    counter += 1;
+}
+counter = 1;
+formatSentenses.forEach((sentense) => console.log(`${counter++}. ${sentense}`));
