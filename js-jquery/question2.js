633333bb181a30e3d095de774536724e9d16bd0b
bhaveshlokhande-000 <72037462+bhaveshlokhande-000@users.noreply.github.com>
2021-07-29 10:21:54 +0530

    question2
    sort object

---
+++ js-jquery/question2.js
@@ -0,0 +1,24 @@
+let qTeams = {
+    aaiec: 400,
+    aaiwc: 60,
+    qai: 200,
+    poc: 100,
+    gtm: 50,
+    hr: 10
+};
+
+var qTeamEntries = [];
+for (var qTeam in qTeams) {
+    qTeamEntries.push([qTeam, qTeams[qTeam]]);
+}
+
+qTeamEntries.sort(function (a, b) {
+    return a[1] - b[1];
+});
+
+var qTeamsSorted = {}
+qTeamEntries.forEach(function (qTeam) {
+    qTeamsSorted[qTeam[0]] = qTeam[1]
+});
+
+//console.log(qTeamsSorted)
